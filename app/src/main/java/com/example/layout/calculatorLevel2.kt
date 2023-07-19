57dc8b643a9508b957349873da05b92586c67727
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-07-20 00:11:57 +0900

    Calculator_level1 코드 수정 & Calculator_level2 코드 작성

--- app/src/main/java/com/example/layout/calculatorLevel2.kt
+++ app/src/main/java/com/example/layout/calculatorLevel2.kt
@@ -1,6 +1,6 @@
 package com.example.layout
 
-//Lv1 : 더하기, 빼기, 나누기, 곱하기 연산을 수행할 수 있는 Calculator 클래스를 만들고, 클래스를 이용하여 연산을 진행하고 출력하기
+//Lv2 : Lv1에서 만든 Calculator 클래스에 나머지 연산을 가능하도록 코드를 추가하고, 연산 진행 후 출력하기
 fun main() {
 
     println("값을 하나씩 입력해 주세요")
@@ -8,11 +8,11 @@ fun main() {
     val operator = readLine()!!.toString()
     val b = readLine()!!.toInt()
 
-    Calculator1(a, operator, b).operator()
+    Calculator2(a, operator, b).operator()
 
 }
 
-class Calculator1(val num1: Int, val operator1: String, val num2: Int) {
+class Calculator2(val num1: Int, val operator1: String, val num2: Int) {
 
     fun operator() {
 
@@ -33,12 +33,16 @@ class Calculator1(val num1: Int, val operator1: String, val num2: Int) {
                 println("$num1 / $num2 의 몫은 ${num1 / num2} 입니다.")
             }
 
+            "%" -> {
+                println("$num1 / $num2 의 나머지는 ${num1 % num2} 입니다.")
+            }
+
             else -> {
                 println("수식이 잘못 입력 되었습니다")
             }
+
         }
 
     }
-
 
 }
